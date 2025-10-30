d8112b9d146b8f1f761be7b481d23f4e453aee32
Surraj Shrikaant Khopkar <contact@surrajkhopkar.com>
2025-10-30 13:47:10 +0530

    added basic concepts

--- handling_checkbox_dynamically.py
+++ handling_checkbox_dynamically.py
@@ -10,7 +10,7 @@ time.sleep(5)
 checkboxes = driver.find_elements(By.CSS_SELECTOR,"input[type='checkbox']")
 print(len(checkboxes))
 for checkbox in checkboxes:
-    if checkbox.get_attribute("value") == "o    ption2":
+    if checkbox.get_attribute("value") == "option2":
         checkbox.click()
         break
 time.sleep(5)
